538b74b598695f51b41fa2db13596e8641c6b7e4
petiteaiueo@gmail.com <petiteaiueo@gmail.com@fb413db2-8c8c-e032-2316-69982d9a617c>
2013-03-12 01:37:19 +0000

    Initiialize moodle and moodledata: - config.php sudah di edit wwwroot nya - sudah di beri sertifikat

---
+++ trunk/moodle/config.php
@@ -0,0 +1,33 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mysqli';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'moodle';
+$CFG->dbuser    = 'root';
+$CFG->dbpass    = '';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbsocket' => 0,
+);
+
+//$CFG->wwwroot   = 'http://localhost/moodle';
+$CFG->wwwroot	= 'http://192.168.1.4/moodle';
+//$CFG->wwwroot   = 'http://172.19.124.39/moodle';
+//$CFG->wwwroot	= 'http://10.151.43.218/moodle';
+$CFG->dataroot  = 'C:\\xampp\\moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+$CFG->passwordsaltmain = '>=6W{18=sVhepsu%f+QcE0q}4ep';
+
+require_once(dirname(__FILE__) . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
